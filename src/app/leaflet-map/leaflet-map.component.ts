3ad1d7d32c6d1fc99b92d7e2bb6f89f891ef8fb5
BrsJsk <joskicboris95@gmail.com>
2019-09-25 09:39:54 +0200

    1 file modified, 1 deleted - rendering markers

--- src/app/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -10,6 +10,17 @@ import { map } from 'leaflet';
 export class LeafletMapComponent implements AfterViewInit {
   map: any;
 
+  markerLocations = [
+    {
+      lat: 38.914764662971436,
+      lng: -97.01461108304595
+    },
+     {
+       lat: 2,
+       lng: 2
+     }
+  ]
+
   constructor() { }
 
   ngAfterViewInit() {
@@ -18,6 +29,29 @@ export class LeafletMapComponent implements AfterViewInit {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.map);
+
+    setTimeout(() => {
+      this.addMarkers()
+    }, 1000)
+  }
+
+  addMarkers() {
+    const icon = L.icon({
+        iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/images/marker-icon.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.5.1/images/marker-shadow.png',
+
+        iconSize: [41, 41], // size of the icon
+        shadowSize: [41, 41], // size of the shadow
+        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62], // the same for the shadow
+        popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+      });
+
+    this.markerLocations.forEach(t => {
+      L.marker([t.lat, t.lng], { icon })
+          .addTo(this.map)
+          .bindPopup('Hi!!');
+    })
   }
 
 }
