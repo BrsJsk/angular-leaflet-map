f50c141a01df1aa57fab64dd46225e1a2974937a
BrsJsk <joskicboris95@gmail.com>
2019-09-25 09:28:42 +0200

    4 files modified - displaying map

--- src/app/leaflet-map/leaflet-map.component.ts
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+import { map } from 'leaflet';
 
 @Component({
   selector: 'app-leaflet-map',
   templateUrl: './leaflet-map.component.html',
   styleUrls: ['./leaflet-map.component.css']
 })
-export class LeafletMapComponent implements OnInit {
+export class LeafletMapComponent implements AfterViewInit {
+  map: any;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+   this.map = map('map').setView([38.914764662971436, -97.01461108304595], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
   }
 
 }
